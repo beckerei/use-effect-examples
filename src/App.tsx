bb718b479d4ea0bed937834e2e64ec34f667228f
Flo <flo@die-beckerei.de>
2020-02-01 23:53:43 +0100

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Polling } from './Polling';
 import './main.css';
 
+type Routes = 'home' | 'polling';
+
 const App = () => {
-  return <div></div>;
+  const [route, setRoute] = useState<Routes>('home');
+
+  const renderRoutes = useCallback(() => {
+    switch(route) {
+      case 'polling': return <Polling />;
+      case 'polling':
+      default: return null;
+    }
+  }, [route]);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setRoute('home')}>Home</button>
+        <button onClick={() => setRoute('polling')}>polling with rxjs</button>
+      </div>
+      <div>{renderRoutes()}</div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Polling/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Polling: React.FC = () => {
+
+    return <>Polling</>
+};
+
