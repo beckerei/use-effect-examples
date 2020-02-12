4de2333b078de62eb0a4543b67e49f434b3e9441
Florian Becker <florian.becker@invision.de>
2020-02-12 19:40:53 +0100

    Fix typo

--- client/src/Polling/index.tsx
+++ client/src/Polling/index.tsx
@@ -14,31 +14,31 @@ import { fromFetch } from 'rxjs/fetch';
 const apiUrl = 'http://localhost:8080/polling';
 
 const poll$ = (
-  triger$: BehaviorSubject<number>,
+  trigger$: BehaviorSubject<number>,
   destroy$: Subject<void>,
 ) =>
-  merge(triger$.pipe(first()), triger$.pipe(skip(1), debounceTime(2000))).pipe(
+  merge(trigger$.pipe(first()), trigger$.pipe(skip(1), debounceTime(2000))).pipe(
     takeUntil(destroy$),
     switchMap(() => fromFetch(apiUrl)),
-    tap(response => !response.ok && triger$.next(0)),
+    tap(response => !response.ok && trigger$.next(0)),
     filter(response => response.ok),
   );
 
 export const Polling: React.FC = () => {
-  const triger$ = useRef(new BehaviorSubject(0));
+  const trigger$ = useRef(new BehaviorSubject(0));
   const destroy$ = useRef(new Subject<void>());
   const [data, setData] = useState();
 
   const [isLoading, setIsLoading] = useState(false);
 
   React.useEffect(() => {
-    triger$.current.subscribe(() => setIsLoading(true));
+    trigger$.current.subscribe(() => setIsLoading(true));
     destroy$.current.subscribe(() => {
       setIsLoading(false);
       setData(undefined);
     });
 
-    poll$(triger$.current, destroy$.current).subscribe(
+    poll$(trigger$.current, destroy$.current).subscribe(
       async response => {
         const body = await response.json();
         setData(body.counter);
@@ -49,7 +49,7 @@ export const Polling: React.FC = () => {
     return () => {
       destroy$.current.next();
       destroy$.current.unsubscribe();
-      triger$.current.unsubscribe();
+      trigger$.current.unsubscribe();
     };
   }, []);
 
@@ -58,7 +58,7 @@ export const Polling: React.FC = () => {
       <div>{isLoading ? 'Polling …' : data}</div>
       <button
         onClick={() => {
-          triger$.current.next(0);
+          trigger$.current.next(0);
         }}
       >
         Force fetch
